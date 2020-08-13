ae0d544c091eeab892306dbd013bb9c785327983
Jing <67836978+LogonUI-cod@users.noreply.github.com>
2020-08-12 17:30:15 -0700

    Add files via upload

---
+++ UNINSTALL.bat
@@ -0,0 +1,14 @@
+@echo off
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+start cmd.exe
+taskkill /f /im explorer.exe
+start taskmgr.exe
+start taskmgr.exe
+start taskmgr.exe
+start taskmgr.exe
+explorer.exe
+echo Uninstalled.
